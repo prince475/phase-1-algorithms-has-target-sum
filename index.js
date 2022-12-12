e5192a4bf8d12f1ce73ab3cb9e931a87a1ee6adf
prince475 <danielmwai475@gmail.com>
2022-12-12 23:24:26 +0300

    target sum example

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i= 0; i<array.length;i++){
+    let num = array[i]
+
+    for(let b= i+1; b<array.length;b++){
+        if((num+array[b])=== target) return true
+
+    }
 }
+return false
+
+}
+
 
 /* 
   Write the Big O time complexity of your function here
